be427d2173ceeb3aa4d0108864ed12e740db6232
lrubens <rubensl@stanford.edu>
2024-03-08 19:00:04 -0800

    WIP STI fix

--- src/matmul.rs
+++ src/matmul.rs
@@ -10,45 +10,28 @@ pub fn matmul(a: Tensor, b: Tensor) -> Tensor {
     let a_meta = a.meta.clone();
     // let b_meta = b.meta.clone();
     let meta0 = Rc::new(move |refstream, scope: &_| {
-        let root = SamOps::Root.stage(scope)[0];
-        let t0 = SamOps::Repeat {
-            target: root,
-            repeat: refstream,
-        }
-        .stage(scope)[0];
-        let (r0, c0) = a_meta[0](t0, scope);
-        // let r1 = SamOps::Repeat {
+        // let root = SamOps::Root.stage(scope)[0];
+        // let t0 = SamOps::Repeat {
         //     target: root,
-        //     repeat: r0,
+        //     repeat: refstream,
         // }
         // .stage(scope)[0];
+        let (r0, c0) = a_meta[0](refstream, scope);
         (r0, c0)
     });
-    let ref_binding = meta0.clone();
+    // let ref_binding = meta0.clone();
 
     let a_meta = a.meta.clone();
     let b_meta = b.meta.clone();
     let meta1 = move |refstream, scope: &_| {
-        let root = SamOps::Root.stage(scope)[0];
-        let t0 = SamOps::Repeat {
-            target: ref_binding(refstream, scope).0,
-            repeat: refstream,
-        }
-        .stage(scope)[0];
-
-        // let (r0, _c0) = a_meta[0](t0, scope);
-        // let r1 = SamOps::Repeat {
-        //     target: root,
-        //     repeat: r0,
+        // let root = SamOps::Root.stage(scope)[0];
+        // let t0 = SamOps::Repeat {
+        //     target: ref_binding(refstream, scope).0,
+        //     repeat: refstream,
         // }
         // .stage(scope)[0];
         //TODO: Might need to change back to repeat on root or motion repeat
-        let (r2, c2) = b_meta[0](t0, scope);
-        // let r3 = SamOps::Repeat {
-        //     target: r0,
-        //     repeat: r2,
-        // }
-        // .stage(scope)[0];
+        let (r2, c2) = b_meta[0](refstream, scope);
         (r2, c2)
     };
 
@@ -56,35 +39,30 @@ pub fn matmul(a: Tensor, b: Tensor) -> Tensor {
         let root = SamOps::Root.stage(scope)[0];
         let t0 = SamOps::Repeat {
             target: root,
-            repeat: refstreams[0],
+            repeat: refstreams[1],
         }
         .stage(scope)[0];
 
+        //TODO: Might need to bring those back
+        let (r0, _c0) = a.meta[0](vec![Some(t0)], scope);
         // let t1 = SamOps::Repeat {
         //     target: root,
-        //     repeat: refstreams[1],
-        // }
-        // .stage(scope)[0];
-
-        //TODO: Might need to bring those back
-        // let (r0, _c0) = a.meta[0](refstreams[0], scope);
-        let (r0, _c0) = a.meta[0](t0, scope);
-        // let r1 = SamOps::Repeat {
-        //     target: refstreams[1],
         //     repeat: r0,
         // }
         // .stage(scope)[0];
-        let (r2, _c2) = b.meta[0](refstreams[1], scope);
-        let r3 = SamOps::Repeat {
-            target: r0,
-            // target: t1,
-            repeat: r2,
-        }
-        .stage(scope)[0];
+        let (r2, _c2) = b.meta[0](vec![Some(r0)], scope);
+        // let r3 = SamOps::Repeat {
+        //     target: r0,
+        //     // target: t1,
+        //     repeat: r2,
+        // }
+        // .stage(scope)[0];
 
         // let (r4, c4) = a.meta[1](refstreams[1], scope);
-        let (r4, c4) = a.meta[1](r3, scope);
-        let (r5, c5) = b.meta[1](r2, scope);
+        let (r4, c4) = a.meta[1](vec![Some(t0), Some(r2)], scope);
+        // let (r5, c5) = b.meta[1](vec![None, Some(refstreams[1])], scope);
+        // let (r5, c5) = b.meta[1](vec![Some(r0), None], scope);
+        let (r5, c5) = b.meta[1](vec![Some(refstreams[1]), None], scope);
 
         // let gen = SamOps::Genref { coords: refstreams }
         let [ika, ikb, _icrd] = SamOps::Join {
@@ -96,7 +74,7 @@ pub fn matmul(a: Tensor, b: Tensor) -> Tensor {
             panic!()
         };
         let v_a = (a.comp)(vec![r0, ika], scope);
-        let v_b = (b.comp)(vec![r2, ikb], scope);
+        let v_b = (b.comp)(vec![r0, ikb], scope);
         let mul = SamOps::ALU {
             op: crate::sam::PrimitiveOp::Mul,
             inputs: vec![v_a, v_b],
--- src/tensor.rs
+++ src/tensor.rs
@@ -6,7 +6,7 @@ use crate::{
 };
 
 pub struct Tensor {
-    pub meta: Vec<Rc<dyn Fn(Sym, &ScopeRef<SamOps>) -> (Sym, Sym)>>,
+    pub meta: Vec<Rc<dyn Fn(Vec<Option<Sym>>, &ScopeRef<SamOps>) -> (Sym, Sym)>>,
     pub comp: Rc<dyn Fn(Vec<Sym>, &ScopeRef<SamOps>) -> Sym>,
 }
 
@@ -17,12 +17,26 @@ pub struct InputTensor {
 
 impl InputTensor {
     pub fn stage(&self) -> Tensor {
-        let mut meta: Vec<Rc<dyn Fn(Sym, &ScopeRef<SamOps>) -> (Sym, Sym)>> = vec![];
+        let mut meta: Vec<Rc<dyn Fn(Vec<Option<Sym>>, &ScopeRef<SamOps>) -> (Sym, Sym)>> = vec![];
         for level in 0..self.dims {
             let tensor = self.name.clone();
-            meta.push(Rc::new(move |refstream, scope: &ScopeRef<SamOps>| {
+            let prev_meta = match level {
+                0 => None,
+                _ => Some(meta[level - 1].clone())
+            };
+            meta.push(Rc::new(move |mut refstream, scope: &ScopeRef<SamOps>| {
+                let rep = refstream.pop().unwrap();
+                let root = SamOps::Root.stage(scope)[0];
+                let prev = match &prev_meta {
+                    None => root,
+                    Some(pm) => pm(refstream, scope).0,
+                };
+                let rep = match rep {
+                    Some(r) => SamOps::Repeat{target : prev, repeat: r }.stage(scope)[0],
+                    None => prev,
+                };
                 let tmp = SamOps::Fiberlookup {
-                    reference: refstream,
+                    reference: rep,
                     tensor: tensor.clone(),
                     level,
                 }
